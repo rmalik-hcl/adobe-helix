e0e9c1f159d2422cb2700bd67bfdaa5447bc5531
Runit Malik <runit.mallik@hcl.com>
2023-06-20 00:12:34 -0700

    updating teaser

--- blocks/teaser/teaser.js
+++ blocks/teaser/teaser.js
@@ -1,6 +1,18 @@
-/**
- * @param {HTMLElement} $block
- */
-export default function decorate() {
+import { createOptimizedPicture } from '../../scripts/lib-franklin.js';
 
+export default function decorate(block) {
+  /* change to ul, li */
+  const ul = document.createElement('ul');
+  [...block.children].forEach((row) => {
+    const li = document.createElement('li');
+    li.innerHTML = row.innerHTML;
+    [...li.children].forEach((div) => {
+      if (div.children.length === 1 && div.querySelector('picture')) div.className = 'story-cover';
+      else div.className = 'overlay pm-blue';
+    });
+    ul.append(li);
+  });
+  ul.querySelectorAll('img').forEach((img) => img.closest('picture').replaceWith(createOptimizedPicture(img.src, img.alt, false, [{ width: '750' }])));
+  block.textContent = '';
+  block.append(ul);
 }

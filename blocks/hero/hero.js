ca40b3ed3e5c2ebcc30b82f43ec5bf1f11257313
Runit Malik <runit.mallik@hcl.com>
2023-06-20 11:26:34 -0700

    updated header and banner

---
+++ blocks/hero/hero.js
@@ -0,0 +1,19 @@
+import { createOptimizedPicture } from '../../scripts/lib-franklin.js';
+
+export default function decorate(block) {
+  /* change to ul, li */
+  const ul = document.createElement('div');
+  ul.className = 'banner';
+  [...block.children].forEach((row) => {
+    const li = document.createElement('div');
+    li.innerHTML = row.innerHTML;
+    [...li.children].forEach((div) => {
+      if (div.children.length === 1 && div.querySelector('picture')) div.className = 'banner-image';
+      else div.className = 'pm13-banner-hero-full';
+    });
+    ul.append(li);
+  });
+  ul.querySelectorAll('img').forEach((img) => img.closest('picture').replaceWith(createOptimizedPicture(img.src, img.alt, false, [{ width: '750' }])));
+  block.textContent = '';
+  block.append(ul);
+}
